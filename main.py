9e7cab24edff64257f74ae4c193512169230a796
exa18 <julian.cenkier@wp.eu>
2022-12-06 09:45:29 +0100

    restore
    Restore state for bot which had to give/remove users from roles.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/bin/python3
-
+#
+#   GIVE/REMOVE role from users
+#
+#
 import discord
 import asyncio
-import datetime
 
-from discord.ext import commands, tasks
+from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 
@@ -14,121 +16,17 @@ intents.members = True
 load_dotenv()
 
 PREFIX = '!r'
+helptxt = "Help: "+PREFIX+"?"
 #
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 #
-#	support
-#
-class WScont:
-	def __init__(self):
-		self.name = "Help: "+PREFIX+"?"
-		self.count = 0
-		self.channel = ''
-		self.start = datetime.datetime.now()
-		self.lasts = 6*(3600*24)
-		self.role = None
-
-def grTimeLeft():
-	l = ws.lasts
-	s = ws.start
-	c = grGetTimeStamp()
-	t = (l+s) - c
-	if t < 0:
-		pass
-	else:
-		show = ''
-		dd = int( (t / 3600) / 24)
-		dh = int( (t - (dd * 24 * 3600))/3600 )
-		dm = int( ((t - (dh * 3600 + dd * 24 * 3600))/3600)*60 )
-
-		if dd > 0:
-			show = str(dd)+"D "
-		if dh > 0:
-			show = show + str(dh) + "h "
-		if dm > 0:
-			show = show + str(dm) + "m "
-		return str(show)
-
-def grCheckTime():
-	l = ws.lasts
-	s = ws.start
-	c = grGetTimeStamp()
-	t = (l+s) - c
-	if t > 0:
-		return True
-	else:
-		return False
-
-def grGetTimeStamp():
-	return datetime.datetime.now().timestamp()
-#
-#
-#	Loop to update status
-#
-@tasks.loop(seconds=15)
-async def task_loop():
-	global ws
-	status = ws.name
-	if ws.count > 0:
-		if grCheckTime() == True:
-			status = ws.name + ". " + grTimeLeft()
-		else:
-			removerole(ws.channel,ws.role)
-			await asyncio.sleep(0)
-			ws = WScont()
-			status = ws.name
-	await bot.change_presence(activity=discord.Game(name=status))
-#
-#	Start
-#
-ws = WScont()
-#
 @bot.event
 async def on_ready():
-	await bot.change_presence(activity=discord.Game(name=ws.name))
-	task_loop.start()
-#
-#
-#	WS Counter: SETs new game name
-#
-#
-@bot.command(name='name')
-async def wsName(ctx, *, arg=''):
-#	info = ""
-#	if ws.count > 0:
-#		ws.start = grGetTimeStamp()
-#		info = "\nGame counter restarted!"
-	if ws.count > 0:
-		if str(arg) == "":
-			arg = ws.name
-		ws.name = str(arg)
-		await ctx.send("...Sets\nGame title: " + arg)
-#
-#
-#	WS Counter: SETs date&time to current (NOW)
-#
-#
-@bot.command(name='time')
-async def wsTime(ctx, *, arg=''):
-	if ws.count > 0:
-		ws.start = grGetTimeStamp()
-		await ctx.send("...Sets\nGame counter restarted!")
-#
-#
-#	WS cunter: Stop
-#
-#
-@bot.command(name='stop')
-async def wsStop(ctx):
-	global ws
-	if ws.count > 0:
-		await ctx.send("...Sets\nGame removed/cleared!")
-		ws = WScont()
+	await bot.change_presence(activity=discord.Game(name=helptxt))
 #
 #
 #	GIVE/ADD role to users
 #
-#	- set WS counter
 #
 @bot.command(name='+')
 async def giverole(ctx, role: discord.Role, *users: discord.Member):
@@ -146,28 +44,13 @@ async def giverole(ctx, role: discord.Role, *users: discord.Member):
 			await ctx.send("...\n" + ", ".join(u) + "\nAdded **" + str(i) + "** users to role **" + str(role) + "**!")
 		else:
 			await ctx.send("Role **" + str(role) + "** already given!")
-	#
-	#	start WS counter
-	#
-	x=0
-	users = bot.get_all_members()
-	for user in users:
-		if role in user.roles:
-			x+=1
-	
-	if ws.count != x or i > 0:
-		ws.name = "WS-" + str(int(x/5)*5)
-		ws.channel = ctx.message.channel.id
-		ws.start = grGetTimeStamp()
-		ws.role = role
-		ws.count = x
-		await ctx.send("Game updated: **"+ ws.name + "**, time till end: " + grTimeLeft() )
+	else:
+		await ctx.send("Provide users for role **" + str(role) + "** to give!")
 #
 #
 #	REMOVE role from users
 #	if arg USERS is empty search in all
 #
-#	- clear WS counter
 #
 @bot.command(name='-')
 async def removerole(ctx, role: discord.Role, *users: discord.Member):
@@ -191,7 +74,6 @@ async def removerole(ctx, role: discord.Role, *users: discord.Member):
 #
 #	SHOW useres assigned to role
 #
-#	- show WS status
 #
 @bot.command(name='s')
 async def showrole(ctx, role: discord.Role):
@@ -203,12 +85,9 @@ async def showrole(ctx, role: discord.Role):
 			u.append(user.name)
 			i+=1
 
-	info = "\nGame slot is empty."
 	if i > 0:
 		u.sort(key=str.lower)
-		if ws.count > 0:
-			info = "\nIn game: **"+ ws.name + "**, time till end: " + grTimeLeft()
-		await ctx.send("...\n" + "\n".join(u) + "\nAssigned **" + str(i) + "** users to role **" + str(role) + "**!"+info)
+		await ctx.send("...\n" + "\n".join(u) + "\nAssigned **" + str(i) + "** users to role **" + str(role) + "**!")
 	else:
 		await ctx.send("Role **" + str(role) + "** is empty!"+info)
 #
@@ -218,14 +97,8 @@ async def showrole(ctx, role: discord.Role):
 async def help(ctx):
 	h = ("\n** **\n\n**Simple bot to give or remove ROLE** for selected users.\n\n" \
 	+ PREFIX +"+ @ROLE @user1 @user2 ... @user11 -> will give role to users \n" \
-	+ PREFIX +"+ @ROLE -> updates game status if users change\n" \
 	+ PREFIX +"- @ROLE [@user1 @user2 ... @user11] -> will remove it\n" \
 	+ PREFIX +"s @ROLE -> show users assigned also current counter\n" \
-	+"\n**WS commands for started counter**\n" \
-	+"...IF counter is set and time counter goes 0 then role will be removed from users\n" \
-	+ PREFIX +"name -> change game name\n" \
-	+ PREFIX +"time -> sets data&time to current (now)\n" \
-	+ PREFIX +"stop -> clears game counter so now to autoremove need to be set again\n" \
 	+ "\nEnjoy!\n"
 	)
 	await ctx.send(h)
